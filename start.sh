0e54e0475fea0fdbb37e83ae885a586b8fcce86d
andrearosasco <andrearosasco.ar@gmail.com>
2023-02-21 18:09:09 +0100

    added start and stop scripts

--- start.sh
+++ start.sh
@@ -1,26 +1,40 @@
-#!/usr/bin/bash
-conda_bin="/home/sberti/miniconda3/envs/ecub/bin/"
+#!/bin/bash
 
-# Stop all existing docker containers and remove them
-docker stop ergocub_container && docker rm ergocub_container
-
-# Start the container with the right options
-docker run --gpus=all -v $(pwd):/home/ecub -itd --rm --network host --name ergocub_container andrewr96/ecub-env:yarp bash
+docker run -itd --rm --network=host --ipc=host \
+  --env DISPLAY=:0 --env QT_X11_NO_MITSHM=1 --env XDG_RUNTIME_DIR=/root/1000 --env XAUTHORITY=/home/btuc/.Xauthority \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ${XAUTHORITY}:/home/btuc/.Xauthority:rw \
+  -v /home/aros/projects/ergocub/ergocub-behavior:/home/btuc/ergocub-behavior \
+  --name ergocub_behavior_container \
+  ar0s/bt-assignment bash
 
 # Create tmux session
-tmux new-session -d -s ecub-tmux
-
-# Souce
+tmux new-session -d -s behavior-tmux
 
 # 0
-tmux send-keys -t ecub-tmux "docker exec -it ergocub_container bash" Enter
-tmux send-keys -t ecub-tmux "/home/btuc/bt-assignment/build/bin/action_recognition" Enter
-tmux split-window -v -t ecub-tmux
+  tmux send-keys -t behavior-tmux "docker exec -it ergocub_behavior_container bash" Enter
+  tmux send-keys -t behavior-tmux "yarp conf 10.0.0.150 10000" Enter
+  tmux send-keys -t behavior-tmux "yarp detect --write" Enter
+  tmux send-keys -t behavior-tmux "/home/btuc/ergocub-behavior/build/bin/object_detection" Enter
+
+tmux split-window -h -t behavior-tmux
 
 # 2
-tmux send-keys -t ecub-tmux "docker exec -it ergocub_container bash" Enter
-tmux send-keys -t ecub-tmux "/home/btuc/bt-assignment/build/bin/object_detection" Enter
-tmux split-window -v -t ecub-tmux
+  tmux send-keys -t behavior-tmux "docker exec -it ergocub_behavior_container bash" Enter
+  tmux send-keys -t behavior-tmux "sleep 5" Enter
+  tmux send-keys -t behavior-tmux "/home/btuc/ergocub-behavior/build/bin/run_bt" Enter
 
+tmux select-pane -t behavior-tmux:0.0
+tmux split-window -v -t behavior-tmux
 
-tmux a -t ecub-tmux
+#
+  tmux send-keys -t behavior-tmux "docker exec -it ergocub_behavior_container bash" Enter
+
+#  tmux send-keys -t behavior-tmux "sleep 5" Enter
+#  tmux send-keys -t behavior-tmux "./grasp-demo /robotology-superbuild/build/install/share/iCub/robots/iCubGazeboV2_7/model.urdf" Enter
+#
+#tmux select-pane -t behavior-tmux:0.2
+#tmux split-window -v -t behavior-tmux
+#
+#tmux send-keys -t behavior-tmux "docker exec -it ergocub_manipulation_container bash" Enter
+
+tmux a -t behavior-tmux
