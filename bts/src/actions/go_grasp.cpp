f4f3c3032a699537c492a7c3e70a5d3207adb31b
Andrea Rosasco <andrearosasco.ar@gmail.com>
2023-05-08 08:28:12 +0000

    added perception client in go_grasp

--- bts/src/actions/go_grasp.cpp
+++ bts/src/actions/go_grasp.cpp
@@ -18,17 +18,30 @@ bool GoGrasp::init(std::string name)
 {
     // MANIPULATION
     #ifdef MANIPULATION
-    std::string server_name = "/Components/Manipulation"s;
-    std::string client_name = "/BT/" + name + "/Manipulation"s;
+    std::string manipulation_server_name = "/Components/Manipulation"s;
+    std::string manipulation_client_name = "/BT/" + name + "/Manipulation"s;
 
-    client_port.open(client_name);
+    manipulation_client_port.open(manipulation_client_name);
 
-    if (!yarp.connect(client_name,server_name))
+    if (!yarp.connect(manipulation_client_name,manipulation_server_name))
     {
-        throw BT::RuntimeError("Error! Could not connect to server ", server_name);
+        throw BT::RuntimeError("Error! Could not connect to server ", manipulation_server_name);
     }
-    manipulation_client_.yarp().attachAsClient(client_port);
+    manipulation_client_.yarp().attachAsClient(manipulation_client_port);
     #endif
+
+    // PERCEPTION
+    std::string perception_server_name = "/eCubPerception/rpc:i"s;
+    std::string perception_client_name = "/BT/" + name + "/eCubPerception"s;
+
+    perception_client_port.open(perception_client_name);
+
+    if (!yarp.connect(perception_client_name,perception_server_name))
+    {
+        std::cout << "Error! Could not connect to server " << perception_server_name << '\n';
+        return false;
+    }
+    ecub_perception_client_.yarp().attachAsClient(perception_client_port);
     return true;
 }
 
