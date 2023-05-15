651fd9cd2b408c243f7ff9ea6237b79fa350da77
Andrea Rosasco <andrearosasco.ar@gmail.com>
2023-05-15 07:01:52 +0000

    removed macros

--- bts/src/common.h
+++ bts/src/common.h
@@ -8,13 +8,6 @@ namespace pt = boost::property_tree;
 using namespace BT;
 #pragma once
 
-//#define MANIPULATION  // Comment to not connect to grasping interface
-//#define GAZE  // Comment to not use gaze controller
-//#define EMOTIONS
-
-// #define ICUB
-#define ECUB
-
 bool are_all_elements_minus_one(const std::vector<double>& vec);
 bool are_all_elements_zero(const std::vector<double>& vec);
 bool are_all_elements_minus_two(const std::vector<double>& vec);
